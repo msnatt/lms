5942be22cc834649a0fd6aea6e5ed4c085be7749
msnatt <nattawat.mue.63@ubu.ac.th>
2025-03-06 11:28:53 +0700

    add mysql

---
+++ lms_e_learning.sql
@@ -0,0 +1,287 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 06, 2025 at 05:28 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lms_e_learning`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `content`
+--
+
+CREATE TABLE `content` (
+  `id` int(11) NOT NULL,
+  `unit_id` int(11) NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `create_date` date NOT NULL DEFAULT current_timestamp(),
+  `update_date` date NOT NULL DEFAULT current_timestamp(),
+  `is_deleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `content`
+--
+
+INSERT INTO `content` (`id`, `unit_id`, `type_id`, `content`, `create_date`, `update_date`, `is_deleted`) VALUES
+(1, 1, 1, 'Geo Unit 1 \r\nContent of Geo Unit 1 with Text', '2025-03-06', '2025-03-06', 0),
+(2, 2, 1, 'Geo Unit 2 \r\nContent of Geo Unit 2 with Text', '2025-03-06', '2025-03-06', 0),
+(3, 3, 1, 'facebook Unit 1 \r\nContent of facebook Unit 1 with Text', '2025-03-06', '2025-03-06', 0),
+(4, 4, 2, 'facebook Unit 2 \r\nContent of facebook Unit 2 with 3D model', '2025-03-06', '2025-03-06', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL,
+  `code` varchar(10) NOT NULL,
+  `image_code` tinyint(4) NOT NULL DEFAULT 0,
+  `name` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `create_date` date NOT NULL DEFAULT current_timestamp(),
+  `update_date` date NOT NULL DEFAULT current_timestamp(),
+  `create_by` int(11) NOT NULL DEFAULT 0 COMMENT 'create by user_id',
+  `update_by` int(11) NOT NULL DEFAULT 0 COMMENT 'update by user_id',
+  `is_deleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`id`, `code`, `image_code`, `name`, `description`, `create_date`, `update_date`, `create_by`, `update_by`, `is_deleted`) VALUES
+(1, '110210', 1, 'Geolocator', 'This is Description of Geolocator Course', '2025-03-03', '2025-03-03', 0, 0, 0),
+(2, '110222', 1, 'Facebook Course', 'This is Description of Facebook Course\r\n', '2025-03-03', '2025-03-03', 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_student`
+--
+
+CREATE TABLE `course_student` (
+  `id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `unit_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `point` float NOT NULL,
+  `create_date` date NOT NULL DEFAULT current_timestamp(),
+  `update_date` date NOT NULL DEFAULT current_timestamp(),
+  `is_active` tinyint(1) NOT NULL DEFAULT 0,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type`
+--
+
+CREATE TABLE `type` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `create_date` date NOT NULL DEFAULT current_timestamp(),
+  `update_date` date NOT NULL DEFAULT current_timestamp(),
+  `is_deleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`id`, `name`, `create_date`, `update_date`, `is_deleted`) VALUES
+(1, 'Text Box', '2025-03-06', '2025-03-06', 0),
+(2, '3D model', '2025-03-06', '2025-03-06', 0),
+(3, 'Document', '2025-03-06', '2025-03-06', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit`
+--
+
+CREATE TABLE `unit` (
+  `id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `create_date` date NOT NULL DEFAULT current_timestamp(),
+  `update_date` date NOT NULL DEFAULT current_timestamp(),
+  `publish_date` date NOT NULL DEFAULT current_timestamp(),
+  `is_deleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `unit`
+--
+
+INSERT INTO `unit` (`id`, `course_id`, `name`, `create_date`, `update_date`, `publish_date`, `is_deleted`) VALUES
+(1, 1, 'Geolocator Unit 1', '2025-03-06', '2025-03-06', '2025-03-06', 0),
+(2, 1, 'Geolocator Unit 2', '2025-03-06', '2025-03-06', '2025-03-06', 0),
+(3, 2, 'Facebook Course Unit 1', '2025-03-06', '2025-03-06', '2025-03-06', 0),
+(4, 2, 'Facebook Course Unit 2', '2025-03-06', '2025-03-06', '2025-03-06', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `rank` int(11) NOT NULL,
+  `telephone` varchar(10) NOT NULL,
+  `create_date` date NOT NULL DEFAULT current_timestamp(),
+  `update_date` date NOT NULL DEFAULT current_timestamp(),
+  `is_pushhandup` tinyint(1) NOT NULL DEFAULT 0,
+  `is_admin` tinyint(1) NOT NULL DEFAULT 0,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `username`, `password`, `rank`, `telephone`, `create_date`, `update_date`, `is_pushhandup`, `is_admin`, `is_deleted`) VALUES
+(1, 'Adminitrator', 'admin@gmail.com', 'admin', 'admin', 0, '0987654321', '2025-03-03', '2025-03-03', 0, 1, 0),
+(2, 'user', 'user@gmail.com', 'user', 'user', 1, '1234567890', '2025-03-03', '2025-03-03', 0, 0, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `content`
+--
+ALTER TABLE `content`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `type_id_content__id_type` (`type_id`),
+  ADD KEY `unit_id_content__id_unit` (`unit_id`);
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `course_student`
+--
+ALTER TABLE `course_student`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course_id_course_student__id_course` (`course_id`),
+  ADD KEY `unit_id_course_student__id_unit` (`unit_id`),
+  ADD KEY `user_id_course_student__id_user` (`user_id`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `unit`
+--
+ALTER TABLE `unit`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course_id_unit__id_course` (`course_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `content`
+--
+ALTER TABLE `content`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `course_student`
+--
+ALTER TABLE `course_student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `unit`
+--
+ALTER TABLE `unit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `content`
+--
+ALTER TABLE `content`
+  ADD CONSTRAINT `type_id_content__id_type` FOREIGN KEY (`type_id`) REFERENCES `type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `unit_id_content__id_unit` FOREIGN KEY (`unit_id`) REFERENCES `unit` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `course_student`
+--
+ALTER TABLE `course_student`
+  ADD CONSTRAINT `course_id_course_student__id_course` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `unit_id_course_student__id_unit` FOREIGN KEY (`unit_id`) REFERENCES `unit` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_id_course_student__id_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `unit`
+--
+ALTER TABLE `unit`
+  ADD CONSTRAINT `course_id_unit__id_course` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
